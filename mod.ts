6ff95494c31290b693cb6e329d430c396bb14ee9
Kugayama Nana <nonamea.774@gmail.com>
2021-10-22 17:01:17 +0900

    vscode Settings for SW code(ref: https://github.com/microsoft/TypeScript/issues/11781#issuecomment-872329281 )

--- mod.ts
+++ mod.ts
@@ -1,5 +1,10 @@
-function handleRequest(_request) {
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2020" />
+/// <reference lib="webworker" />
+
+function handleRequest(_: Request): Response {
   return new Response('Degraded', { headers: { 'content-type': 'text/plain' }});
 }
 
-addEventListener('fetch', async (event) => { event.respondWith(handleRequest(event.request)) });
+const sw = self as ServiceWorkerGlobalScope & typeof globalThis
+sw.addEventListener('fetch', async (event: FetchEvent) => { event.respondWith(handleRequest(event.request)) });
