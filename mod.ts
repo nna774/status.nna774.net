1815832749df540611b0fcca5b982634d8328f7c
Kugayama Nana <nonamea.774@gmail.com>
2021-10-22 18:27:04 +0900

    swtich accept header

--- mod.ts
+++ mod.ts
@@ -1,7 +1,19 @@
 /// <reference path='./deploy.d.ts' />
 
-function handleRequest(_: Request): Response {
-  return new Response('Degraded', { headers: { 'content-type': 'text/plain' }});
+type Status = {
+  status: 'Degraded' | 'OK';
+};
+
+const responseFromAcceptHeader = (accept: string, s: Status): Response => {
+  const JSONContentType = 'application/json';
+  if (accept.includes(JSONContentType)) {
+    return new Response(`{"status": "${s.status}"}`, { headers: { 'content-type': JSONContentType }});
+  }
+  return new Response(`${s.status}\n`, { headers: { 'content-type': 'text/plain' }});
+}
+
+const handleRequest = (req: Request): Response => {
+  return responseFromAcceptHeader(req.headers.get('Accept') || '', {status: 'Degraded'})
 }
 
 //const sw = self as ServiceWorkerGlobalScope & typeof globalThis
