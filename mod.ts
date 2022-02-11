374009004de98096170ed98d72e54969716c3487
Kugayama Nana <nonamea.774@gmail.com>
2022-02-12 00:03:27 +0900

    favicon / ひみつのこと

--- mod.ts
+++ mod.ts
@@ -10,10 +10,25 @@ const responseFromAcceptHeader = (accept: string, s: Status): Response => {
     return new Response(JSON.stringify(s), { headers: { 'content-type': JSONContentType }});
   }
   return new Response(`${s.status}\n`, { headers: { 'content-type': 'text/plain' }});
-}
+};
+
+const favicon = (): Response => {
+  return new Response(null, {status: 302, headers: { location: 'https://nna774.net/favicon.ico' }});
+};
+
+const ひみつのことResponse = (): Response => {
+  return new Response('今回の「ピ」について', { status: 401 });
+};
 
 const handleRequest = (req: Request): Response => {
+  const url = new URL(req.url);
+  switch(url.pathname) {
+  case '/favicon.ico':
+    return favicon();
+  case encodeURI('/ひみつのこと.txt'):
+    return ひみつのことResponse();
+  }
   return responseFromAcceptHeader(req.headers.get('Accept') || '', {status: 'Degraded'});
-}
+};
 
 addEventListener('fetch', async (event: FetchEvent) => { event.respondWith(handleRequest(event.request)) });
